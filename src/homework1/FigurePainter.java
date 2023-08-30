b9a2157d76699e620d01289e5e4a15ecd8e14d18
Robert Nazaryan <robertnazaryan0@gmail.com>
2023-08-30 18:38:40 +0400

    add 5th exercise

--- src/homework1/FigurePainter.java
+++ src/homework1/FigurePainter.java
@@ -44,5 +44,27 @@ public class FigurePainter {
             }
             System.out.println();
         }
+
+        System.out.println();
+        System.out.println();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5 - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 5 - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
