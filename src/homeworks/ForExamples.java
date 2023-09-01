1c2a68fd375c13d2ff44fd34f33ad86fe8c1ba76
Robert Nazaryan <robertnazaryan0@gmail.com>
2023-09-01 09:58:12 +0400

    add ForExamples class

---
+++ src/homeworks/ForExamples.java
@@ -0,0 +1,33 @@
+package homeworks;
+
+public class ForExamples {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i);
+            if (i != 100) {
+                System.out.print("-");
+            }
+        }
+
+        System.out.println();
+        System.out.println();
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println();
+        System.out.println();
+
+        int[] array = {2,5,8,4,9,3,7};
+        int max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if(max < array[i]){
+                max = array[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
