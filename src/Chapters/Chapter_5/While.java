fdb6295c9db71a513539525279f7857a40932365
Robert Nazaryan <robertnazaryan0@gmail.com>
2023-09-04 19:28:31 +0400

    chapter 5 examples

---
+++ src/Chapters/Chapter_5/While.java
@@ -0,0 +1,12 @@
+package Chapters.Chapter_5;
+
+public class While {
+    public static void main(String[] args) {
+        int n = 10;
+
+        while (n > 0){
+            System.out.println("tick " + n);
+            n--;
+        }
+    }
+}
