14ad0749ed46bf79d1b9b2a833a6f676c59a47cc
Robert Nazaryan <robertnazaryan0@gmail.com>
2023-09-11 21:11:27 +0400

    add Sort class

---
+++ src/homeworks/Sort.java
@@ -0,0 +1,32 @@
+package homeworks;
+
+public class Sort {
+    public static void main(String[] args) {
+        int[] array = {4, 7, 1, 3, 9, 0, 2};
+
+        bubbleSort(array);
+        printArray(array);
+    }
+
+    static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j);
+                }
+            }
+        }
+    }
+
+    static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    static void swap(int[] arr, int i) {
+        int temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+    }
+}
