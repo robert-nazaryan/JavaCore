b2d3319600de97f38b3337ab8892e3115a9e2775
Robert Nazaryan <robertnazaryan0@gmail.com>
2023-09-11 19:52:50 +0400

    add FigurePainter with methods

--- src/homeworks/figurePainter/FigurePainter.java
+++ src/homeworks/figurePainter/FigurePainter.java
@@ -1,40 +1,37 @@
-package homeworks;
+package homeworks.figurePainter;
 
 public class FigurePainter {
-    public static void main(String[] args) {
+    void figureOne() {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
+    }
 
-        System.out.println();
-        System.out.println();
-
-        for (int i = 5; i > 0; i--) {
+    void figureTwo(int n, char c) {
+        for (int i = n; i > 0; i--) {
             for (int j = i; j > 0; j--) {
-                System.out.print("* ");
+                System.out.print(c + " ");
             }
             System.out.println();
         }
+    }
 
-        System.out.println();
-        System.out.println();
-
+    void figureThree(char c) {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5 - i - 1; j++) {
                 System.out.print("  ");
             }
             for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
+                System.out.print(c + " ");
             }
             System.out.println();
         }
+    }
 
-        System.out.println();
-        System.out.println();
-
+    void figureFour() {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j <= i - 1; j++) {
                 System.out.print("  ");
@@ -44,25 +41,24 @@ public class FigurePainter {
             }
             System.out.println();
         }
+    }
 
-        System.out.println();
-        System.out.println();
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 5 - i - 1; j++) {
+    void figureFive(int n, char c) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
             }
             for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
+                System.out.print(c + " ");
             }
             System.out.println();
         }
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i - 1; j++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j < 5 - i; j++) {
-                System.out.print("* ");
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(c + " ");
             }
             System.out.println();
         }
---
+++ src/homeworks/figurePainter/FigurePainterDemo.java
@@ -0,0 +1,13 @@
+package homeworks.figurePainter;
+
+public class FigurePainterDemo {
+    public static void main(String[] args) {
+        FigurePainter fp = new FigurePainter();
+
+        fp.figureThree('+');
+        fp.figureTwo(10, 'H');
+        fp.figureFive(4,'&');
+        fp.figureFive(15, 'W');
+
+    }
+}
