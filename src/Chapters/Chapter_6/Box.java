8abdc1fb9cbbae3da1c11d466dd7fc3dd8ae4418
Robert Nazaryan <robertnazaryan0@gmail.com>
2023-09-19 11:49:14 +0400

    Chapter 6

---
+++ src/Chapters/Chapter_6/Box.java
@@ -0,0 +1,22 @@
+package Chapters.Chapter_6;
+
+public class Box {
+    double width;
+    double height;
+    double depth;
+
+    Box(double width, double height, double depth){
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+    double volume() {
+        return width * height * depth;
+    }
+
+    void setDim(double w, double h, double d){
+        width = w;
+        height = h;
+        depth = d;
+    }
+}
